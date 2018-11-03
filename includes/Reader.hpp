0e15fa0710c60de18bd62561565c3f878b96c05a
iradchenua <oxxxyivan@gmail.com>
2018-11-03 13:27:24 +0200

    add some errors managment, and operation with numbers

--- includes/Reader.hpp
+++ includes/Reader.hpp
@@ -3,27 +3,93 @@
 #include <sstream>
 #include <vector>
 #include <iterator>
+#include <map>
 
 #ifndef READER_HPP
 #define READER_HPP
 
 class Reader {
 	private:
+
+		typedef void (Reader::*noArgumentFun)();
+
+		std::stringstream _messages;
 		std::string _line;
 		bool 		_mute;
 		std::deque<IOperand const *> * const _deque;
+
+		IOperand const *_lhs;
+		IOperand const *_rhs;
+
+		static std::map<std::string, noArgumentFun> _mapNoArgumentFun;
 
 		Reader(Reader const & rhs);
 		Reader();
 		Reader const & operator=(Reader const & rhs);
 
-		void parseLine() const;
+		void parseLine();
 		void getInitializationValue(std::string const & init) const;
-		void dump() const;
+
+		void sub();
+		void add();
+		void mul();
+		void div();
+		void mod();
+		
+		void dump();
+		void pop();
+
+		void searchPosOfType(std::string const & init, std::size_t & pos, int & type) const;
+
+		void checkEmpty() const;
+
+		void getLRhs();
+		void clearLRhs();
+
 	public:
 		Reader(std::deque<IOperand const *> * const deque);
 		~Reader();
 		void read();
+
+
+		class ReaderExcept: public std::exception {
+			public:
+				ReaderExcept(void);
+
+				ReaderExcept(ReaderExcept const & e);
+				~ReaderExcept(void) throw();
+
+				ReaderExcept & operator=(const ReaderExcept & e);
+
+				virtual const char *what() const throw();
+		};
+
+		class InvalidCommand: public ReaderExcept {
+			private:
+				std::string const _invalidCommand;
+			public:
+				InvalidCommand(void);
+
+				InvalidCommand(std::string const & invalidCommand);
+				InvalidCommand(InvalidCommand const & e);
+				~InvalidCommand(void) throw();
+
+				InvalidCommand & operator=(const InvalidCommand & e);
+
+				virtual const char *what() const throw();
+		};
+
+		class EmptyStack: public ReaderExcept {
+			public:
+				EmptyStack(void);
+
+				EmptyStack(EmptyStack const & e);
+				~EmptyStack(void) throw();
+
+				EmptyStack & operator=(const EmptyStack & e);
+
+				virtual const char *what() const throw();
+		};
 };
 
 #endif
--- src/Reader.cpp
+++ src/Reader.cpp
@@ -1,17 +1,26 @@
 #include "Reader.hpp"
+#include <sstream> 
 
 Reader::Reader(std::deque<IOperand const *> * const deque) : _deque(deque) {
 	this->_mute = false;
 	this->_line = "";
+	this->_lhs = nullptr;
+	this->_rhs = nullptr;
 }
 
 Reader::Reader() : _deque(nullptr) {
 	this->_mute = false;
 	this->_line = "";
+	this->_lhs = nullptr;
+	this->_rhs = nullptr;
 }
 
 Reader::~Reader() {
-
+	this->clearLRhs();
+	for (auto operand : *(this->_deque)) {
+		delete operand;
+	}
+	delete this->_deque;
 }
 
 Reader::Reader(Reader const & rhs) : _deque(nullptr) {
@@ -23,40 +32,108 @@ Reader const & Reader::operator=(Reader const & rhs) {
 	return (*this);
 }
 
-void Reader::getInitializationValue(std::string const & init) const {
-	std::size_t pos;
-	int i = Int8;
-
-	for ( ; i < COUNT; i++) {
-		pos = init.find(factorySpace::lowStrTypes[i]);
+void Reader::searchPosOfType(std::string const & init, std::size_t & pos, int & type) const {
+	type = Int8;
+	for ( ; type < COUNT; type++) {
+		pos = init.find(factorySpace::lowStrTypes[type]);
 		if (pos != std::string::npos)
 			break;
 	}
-
-	if (pos == 0) {
-		std::size_t openBracePos = init.find("(");
-		std::size_t closeBracePos = init.find(")");
-
-		if (openBracePos != factorySpace::lowStrTypes[i].size())
-			std::cout << "not valid command" << std::endl;
-		if (closeBracePos == openBracePos + 1)
-			std::cout << "empty value in braces error" << std::endl;
-		if (closeBracePos == std::string::npos)
-		 	std::cout << "missed close brace" << std::endl;
-		if (openBracePos == std::string::npos)
-			std::cout << "missed open brace" << std::endl;
-
-		this->_deque->push_front(factorySpace::factory.createOperand(static_cast<eOperandType>(i), init.substr(openBracePos + 1, closeBracePos - openBracePos - 1)));
-	}
 }
 
-void Reader::dump() const {
+void Reader::getInitializationValue(std::string const & init) const {
+	std::size_t pos;
+	int type;
+
+	this->searchPosOfType(init, pos, type);
+
+	if (pos != 0)
+		throw Reader::InvalidCommand(init);
+
+	std::size_t openBracePos = init.find("(");
+	std::size_t closeBracePos = init.find(")");
+
+	if (openBracePos != factorySpace::lowStrTypes[type].size())
+		throw Reader::InvalidCommand(init);
+	if (closeBracePos == openBracePos + 1)
+		std::cout << "empty value in braces error" << std::endl;
+	if (closeBracePos == std::string::npos)
+	 	std::cout << "missed close brace" << std::endl;
+	if (openBracePos == std::string::npos)
+		std::cout << "missed open brace" << std::endl;
+
+	this->_deque->push_front(\
+		factorySpace::factory.createOperand(static_cast<eOperandType>(type), init.substr(openBracePos + 1, closeBracePos - openBracePos - 1))
+	);
+}
+
+void Reader::checkEmpty() const {
+	if (this->_deque->empty())
+		throw EmptyStack();
+}
+
+void Reader::dump() {
+	std::string wtf;
+
 	for (auto operand : *(this->_deque)) {
-		std::cout << operand->toString() << std::endl;
+		this->_messages << operand->toString() << std::endl;
 	}
 }
 
-void Reader::parseLine() const {
+void Reader::clearLRhs() {
+	if (this->_lhs)
+		delete this->_lhs;
+	if (this->_rhs)
+		delete this->_rhs;
+	this->_lhs = nullptr;
+	this->_rhs = nullptr;
+}
+
+void Reader::getLRhs() {
+	this->checkEmpty();
+	this->_lhs = this->_deque->front();
+	this->_deque->pop_front();
+	this->checkEmpty();
+	this->_rhs = this->_deque->front();
+	this->_deque->pop_front();
+}
+
+void Reader::mod() {
+	this->getLRhs();
+	this->_deque->push_back(*this->_rhs % *this->_lhs);
+	this->clearLRhs();
+}
+
+void Reader::div() {
+	this->getLRhs();
+	this->_deque->push_back(*this->_rhs / *this->_lhs);
+	this->clearLRhs();
+}
+
+void Reader::mul() {
+	this->getLRhs();
+	this->_deque->push_back(*this->_rhs * *this->_lhs);
+	this->clearLRhs();
+}
+
+void Reader::sub() {
+	this->getLRhs();
+	this->_deque->push_back(*this->_rhs - *this->_lhs);
+	this->clearLRhs();
+}
+
+void Reader::add() {
+	this->getLRhs();
+	this->_deque->push_back(*this->_lhs + *this->_rhs);
+	this->clearLRhs();
+}
+
+void Reader::pop() {
+	delete this->_deque->front();
+	this->_deque->pop_front();
+}
+
+void Reader::parseLine()  {
     std::istringstream buf(this->_line);
     std::istream_iterator<std::string> beg(buf), end;
 
@@ -64,27 +141,12 @@ void Reader::parseLine() const {
 
     if (splited.size() >= 1) {
     	std::string const & command = splited[0];
-    	if (command == "pop")
-    		std::cout << "pop staff" << std::endl;
-    	else if (command == "dump")
-    		this->dump();
-    	else if (command == "add")
-    		std::cout << "add" << std::endl;
-    	else if (command == "sub")
-    		std::cout << "sub staff" << std::endl;
-    	else if (command == "mull")
-    		std::cout << "mull staff" << std::endl;
-    	else if (command == "div")
-    		std::cout << "div staff" << std::endl;
-    	else if (command == "mod")
-    		std::cout << "mod staff" << std::endl;
-    	else if (command == "print")
-    		std::cout << "print staff" << std::endl;
-    	else if (command == "push" && splited.size() >= 2) {
+    	if (Reader::_mapNoArgumentFun.count(command))
+    		(this->*Reader::_mapNoArgumentFun[command])();
+    	else if (command == "push" && splited.size() >= 2)
     		getInitializationValue(splited[1]);
-    	}
     	else
-    		std::cout << "error invalid command" << std::endl;
+    		throw Reader::InvalidCommand(command);
 
 		for(std::vector<std::string>::iterator it = splited.begin() + 1; it != splited.end(); it++) {
 		    if (!(*it).empty())
@@ -96,15 +158,15 @@ void Reader::parseLine() const {
 }
 
 void Reader::read() {
+	std::size_t numberLine = 0;
 
 	while (std::cin.good()) {
+
 		getline(std::cin, this->_line);
 		this->_line.erase(0, this->_line.find_first_not_of("\t\v\r\f "));
 
-		if (this->_line == ";;") {
-			std::cout << "end" << std::endl;
+		if (this->_line == ";;")
 			break;
-		}
 
 		if (!this->_mute && !this->_line.empty()) {
 			if (this->_line.at(0) == ';')
@@ -112,9 +174,90 @@ void Reader::read() {
 			else if (this->_line == "exit")
 				this->_mute = true;
 			else {
-				this->parseLine();
+				try {
+					this->parseLine();
+				}
+				catch (Reader::ReaderExcept const & e) {
+					this->clearLRhs();
+					this->_messages << "Line " << numberLine \
+					<< ": Error : " << e.what() << std::endl;
+				}
+				catch (std::exception const & e) {
+					this->clearLRhs();
+					this->_messages << "Line " << numberLine \
+					<< ": Error : " << e.what() << std::endl;
+				}
 			}
 		}
-		//std::cout << line << std::endl;
+		numberLine++;
 	}
+	std::cout << this->_messages.str();
 }
+
+Reader::ReaderExcept & Reader::ReaderExcept::operator=(Reader::ReaderExcept const &e)
+{
+	std::exception::operator=(e);
+	return (*this);
+}
+
+Reader::ReaderExcept::ReaderExcept() {
+
+};
+Reader::ReaderExcept::~ReaderExcept() throw() {};
+
+Reader::ReaderExcept::ReaderExcept(Reader::ReaderExcept const & e) {
+	*this = e;
+}
+
+const char *Reader::ReaderExcept:: what(void) const throw() {
+	return ("");
+}
+
+
+Reader::InvalidCommand	& Reader::InvalidCommand::operator=(Reader::InvalidCommand const &e)
+{
+	std::exception::operator=(e);
+	return (*this);
+}
+
+Reader::InvalidCommand::InvalidCommand(std::string const & invalidCommand) : _invalidCommand("Invalid command: " + invalidCommand) {
+
+};
+
+Reader::InvalidCommand::InvalidCommand() : _invalidCommand("InvalidCommand: ") {
+
+};
+Reader::InvalidCommand::~InvalidCommand() throw() {};
+
+Reader::InvalidCommand::InvalidCommand(Reader::InvalidCommand const & e) {
+	*this = e;
+}
+
+const char *Reader::InvalidCommand:: what(void) const throw() {
+	return (this->_invalidCommand.c_str());
+}
+
+
+Reader::EmptyStack	& Reader::EmptyStack::operator=(Reader::EmptyStack const &e)
+{
+	std::exception::operator=(e);
+	return (*this);
+}
+
+Reader::EmptyStack::EmptyStack() {
+
+};
+Reader::EmptyStack::~EmptyStack() throw() {};
+
+Reader::EmptyStack::EmptyStack(Reader::EmptyStack const & e) {
+	*this = e;
+}
+
+const char *Reader::EmptyStack:: what(void) const throw() {
+	return ("empty stack");
+}
+
+std::map<std::string, Reader::noArgumentFun> Reader::_mapNoArgumentFun = \
+{{"pop", &Reader::pop}, {"dump", &Reader::dump}, {"add", &Reader::add}, \
+  {"sub", &Reader::sub}, {"mul", &Reader::mul}, {"div", &Reader::div}, \
+ {"mod", &Reader::mod}};
